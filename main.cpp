8c1e0274e9cdc5af9c032dc7a59e1eab18d858be
Portalion <77tobik77@gmail.com>
2023-03-10 20:57:02 +0100

    Dodaj pliki projektów.

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#define NOMINMAX
+#include <windows.h>
+
+int WINAPI wWinMain(HINSTANCE instance, HINSTANCE previnstance, LPWSTR cmdline, int show_command)
+{
+	UNREFERENCED_PARAMETER(previnstance);
+	UNREFERENCED_PARAMETER(cmdline);
+
+	return 0;
+}
