1db223b8e72d001a427157c9d495c2abe0ad78ed
Portalion <77tobik77@gmail.com>
2023-03-11 00:48:05 +0100

    add main

--- main.cpp
+++ main.cpp
@@ -3,8 +3,7 @@
 
 int WINAPI wWinMain(HINSTANCE instance, HINSTANCE previnstance, LPWSTR cmdline, int show_command)
 {
-	UNREFERENCED_PARAMETER(previnstance);
-	UNREFERENCED_PARAMETER(cmdline);
+
 
 	return 0;
 }
